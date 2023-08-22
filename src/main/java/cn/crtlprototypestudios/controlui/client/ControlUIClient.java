6750b0376a9b39cf3c0ae6a2d54747e8a36f5633
Type-32 <87076491+Type-32@users.noreply.github.com>
2023-08-22 22:59:55 +0800

    ControlUI UI Lib testing

--- src/main/java/cn/crtlprototypestudios/controlui/client/ControlUIClient.java
+++ src/main/java/cn/crtlprototypestudios/controlui/client/ControlUIClient.java
@@ -2,38 +2,34 @@ package cn.crtlprototypestudios.controlui.client;
 
 // Import Fabric UI libraries
 import cn.crtlprototypestudios.controlui.events.inputs.InputKeyHandler;
+import cn.crtlprototypestudios.controlui.gui.MainGUI;
+import cn.crtlprototypestudios.controlui.gui.MainScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
 
 // Baritone API imports
-import baritone.api.BaritoneAPI;
-import baritone.api.pathing.goals.Goal;
-import baritone.api.process.PathingCommand;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
-import org.jetbrains.annotations.NotNull;
 
 // Minecraft
-import net.minecraft.client.MinecraftClient;
+
 
 @Environment(EnvType.CLIENT)
 public class ControlUIClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
-            while(InputKeyHandler.OPEN_CONTROL_UI.wasPressed()) {
-
+            while(InputKeyHandler.TOGGLE_CONTROL_UI.wasPressed()) {
+                System.out.println("Toggled Control UI");
+                MinecraftClient.getInstance().setScreen(new MainScreen(new MainGUI()));
             }
-            while(InputKeyHandler.OPEN_CONTROL_UI.wasPressed()) {
-
+            while(InputKeyHandler.CANCEL_ALL_ACTIONS.wasPressed()) {
+                System.out.println("Cancelled all actions");
             }
         });
-        KeyBindingHelper.registerKeyBinding(InputKeyHandler.OPEN_CONTROL_UI);
+        KeyBindingHelper.registerKeyBinding(InputKeyHandler.TOGGLE_CONTROL_UI);
         KeyBindingHelper.registerKeyBinding(InputKeyHandler.CANCEL_ALL_ACTIONS);
         KeyBindingHelper.registerKeyBinding(InputKeyHandler.PAUSE_ALL_ACTIONS);
     }
--- src/main/java/cn/crtlprototypestudios/controlui/gui/MainGUI.java
+++ src/main/java/cn/crtlprototypestudios/controlui/gui/MainGUI.java
@@ -1,12 +1,29 @@
 package cn.crtlprototypestudios.controlui.gui;
 import io.github.cottonmc.cotton.gui.client.LightweightGuiDescription;
 import io.github.cottonmc.cotton.gui.widget.WGridPanel;
+import io.github.cottonmc.cotton.gui.widget.WTabPanel;
+import io.github.cottonmc.cotton.gui.widget.WText;
+import io.github.cottonmc.cotton.gui.widget.data.Insets;
+import net.minecraft.text.Text;
 
 public class MainGUI extends LightweightGuiDescription {
     public MainGUI() {
-        WGridPanel root = new WGridPanel();
-        setRootPanel(root);
-        root.setSize(300, 200);
-        root.add(new WGridPanel(), 0, 0, 1, 1);
+        // Add tabs
+        WTabPanel tabs = new WTabPanel();
+        setRootPanel(tabs);
+
+        //Initialize Menus
+        WGridPanel menu = new WGridPanel();
+        menu.setSize(300,200);
+        menu.add(new WText(Text.translatable("gui.control_ui.menus.main.title")), 0, 0);
+
+        // Add Menus to Tabs
+        tabs.add(menu, tab -> tab.title(Text.translatable("gui.control_ui.tabs.main")));
+        tabs.add(menu, tab -> tab.title(Text.translatable("gui.control_ui.tabs.mine")));
+        tabs.add(new WGridPanel(), tab -> tab.title(Text.translatable("gui.control_ui.tabs.goal")));
+        tabs.add(new WGridPanel(), tab -> tab.title(Text.translatable("gui.control_ui.tabs.waypoint")));
+        tabs.add(new WGridPanel(), tab -> tab.title(Text.translatable("gui.control_ui.tabs.settings")));
+
+        tabs.validate(this);
     }
 }
